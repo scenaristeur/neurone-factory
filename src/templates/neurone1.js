f4c94959d259a77d094889cccfc5f8254d6cd766
Scenaristeur <scenaristeur@gmail.com>
2022-01-20 15:35:15 +0100

    create

--- src/templates/neurone1.js
+++ src/templates/neurone1.js
@@ -1,19 +1,19 @@
 export default
 {
-  // "@context": {
-  //   "@vocab": "https://www.w3.org/ns/activitystreams",
-  //   "ve": "https://scenaristeur.github.io/verse/",
-  //   "@id": "id",
-  //   "@type": "type"
-  // },
+  "@context": {
+    "@vocab": "https://www.w3.org/ns/activitystreams",
+    "ve": "https://scenaristeur.github.io/verse/",
+    "@id": "id",
+    "@type": "type"
+  },
   "id": null,
   "ve:name": null ,
   "ve:age": 0 ,
-  // "ve:url": null,
-  // "ve:privacy": 'private',
-  // "ve:type": "node",
-  // "ve:properties": [],
-  // "ve:links": [],
+  "ve:url": null,
+  "ve:privacy": 'private',
+  "ve:type": "node",
+  "ve:properties": [],
+  "ve:links": [],
   "ve:created": null,
   "ve:updated": null,
   "ve:synchronized": null,
